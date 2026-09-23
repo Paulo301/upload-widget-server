878c3063b0fbd5eaee5f07f2799ee0b55df6da73
Paulo Victor Lemos de Almeida <pauloalmeida@ma.def.br>
2026-09-23 10:55:09 -0300

    feat: create uploads route and global error handler

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,37 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 import { env } from '@/env'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  //Envia o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+  console.log(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 server.register(fastifyCors, { origin: '*' })
 
 console.log(env.DATABASE_URL)
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('Http server running!')
